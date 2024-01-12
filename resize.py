9120d7d1d4ba408d6268740e2bedbe549403ae24
Dennis Deng <dennisdeng2002@gmail.com>
2024-01-12 14:46:47 -0500

    Some changes

--- resize.py
+++ resize.py
@@ -3,9 +3,9 @@ import subprocess
 
 input_path = 'original'
 output_paths = ['small', 'medium']
-widths = [300, 600]
-# convert original/ACCR.jpg -resize 600 medium/ACCR.jpg
-resize_template = 'convert {}/{} -resize {} {}/{}'
+widths = [200, 600]
+# convert original/ACCR.jpg -resize 600 -quality 50 medium/ACCR.webp
+resize_template = 'convert {}/{}.jpg -resize {} -quality 50 {}/{}.webp'
 
 for i, output_path in enumerate(output_paths):
     total_count = 0
@@ -18,7 +18,7 @@ for i, output_path in enumerate(output_paths):
     print("Processing {} images".format(output_path))
     for entry in scandir(input_path):
         if entry.path.endswith('.jpg') and entry.is_file():
-            name = entry.name
+            name = entry.name.replace('.jpg', '')
             resize = resize_template.format(input_path, name, width, output_path, name).split()
             subprocess.run(resize)
             print("Processed {}/{}".format(counter, total_count))
