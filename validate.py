08fb27683147bd74a2af8d0b23e8d9b7041ecfa4
Dennis Deng <dennisdeng2002@gmail.com>
2024-01-13 00:10:20 -0500

    Some changes

--- validate.py
+++ validate.py
@@ -7,8 +7,31 @@ with open('casinos.txt', 'r') as file:
     for line in file:
         ids.add(line.split(',')[0])
 
+total = len(ids)
+
+processed_ids = set()
+
+with open('processed_casinos.txt', 'r') as file:
+    for line in file:
+        processed_ids.add(line.strip())
+
 for entry in scandir('original'):
     if entry.path.endswith('.jpg') and entry.is_file():
         id = entry.name.replace('.jpg', '')
         if id not in ids:
             print(id)
+        else:
+            ids.remove(id)
+        
+        if id in processed_ids:
+            processed_ids.remove(id)
+
+    elif not entry.path.endswith('.DS_Store'):
+        print(entry)
+
+known_skipped_ids = {'CAMC', 'GGAM'}
+for id in processed_ids:
+    if id not in known_skipped_ids:
+        print(id)
+
+print("{} / {} remaining".format(len(ids) - len(known_skipped_ids), total))
